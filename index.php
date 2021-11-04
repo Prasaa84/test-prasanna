ee1df872d0a97570a73312362eb2230f1fc0e612
Prasaa84 <93713635+Prasaa84@users.noreply.github.com>
2021-11-04 18:08:28 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Test-Digital Ocean</title>
+	<link rel="stylesheet" type="text/css" href="css/custom.css">
+</head>
+<body>
+	<?php $username = 'Prasanna'; ?>
+	<h1><?php echo 'Hi... '.$username; ?></h1>
+</body>
+</html>
